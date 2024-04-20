005b5f8736621d4968782b17ba078b0fe7cf3263
Jordi <55429631+jorbush@users.noreply.github.com>
2024-04-20 22:13:25 +0200

    Complete the handshake

---
+++ handshake.go
@@ -0,0 +1,37 @@
+package peertopeerclient
+
+import "io"
+
+// A Handshake is a special message that a peer uses to identify itself
+type Handshake struct {
+	Pstr     string // BitTorrent protocol
+	InfoHash [20]byte
+	PeerID   [20]byte
+}
+
+// Serialize serializes the handshake to a buffer
+func (h *Handshake) Serialize() []byte {
+	// Create a buffer with enough space to hold the serialized handshake
+	buf := make([]byte, len(h.Pstr)+49)
+	// The first byte indicates the length of the protocol identifier
+	buf[0] = byte(len(h.Pstr))
+	// Initialize a variable to keep track of the current position in the buffer
+	curr := 1
+	// Copy the protocol identifier (Pstr) into the buffer
+	curr += copy(buf[curr:], h.Pstr)
+	// Copy 8 reserved bytes (all set to 0) into the buffer
+	curr += copy(buf[curr:], make([]byte, 8)) // 8 reserved bytes
+	// Copy the infohash into the buffer
+	curr += copy(buf[curr:], h.InfoHash[:])
+	// Copy the peer ID into the buffer
+	curr += copy(buf[curr:], h.PeerID[:])
+	// Return the serialized buffer
+	return buf
+}
+
+// Read parses a handshake from a stream
+func Read(r io.Reader) (*Handshake, error) {
+	// Do Serialize(), but backwards
+	// ...
+	return &Handshake{}, nil
+}
