b9547217baf63ab60ff25c1c859a4a88ccf06c74
Jordi <55429631+jorbush@users.noreply.github.com>
2024-06-15 12:51:53 +0200

    Drag and drop works

--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"gotorrent/bittorrent/core"
 	"gotorrent/ui"
+	"image/color"
 	"log"
 	"os"
 	"time"
 
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
@@ -35,8 +37,10 @@ func main() {
 }
 
 func startUI() {
+	isDownloading := false
 	myApp := app.New()
 	myWindow := myApp.NewWindow("GoTorrent")
+	myWindow.Resize(fyne.NewSize(600, 400)) // Ajusta el tamaño inicial de la ventana
 
 	label := widget.NewLabel("Drag and drop a torrent file or click to browse")
 	fileEntry := widget.NewEntry()
@@ -45,10 +49,12 @@ func startUI() {
 	fileButton := widget.NewButton("Browse", func() {
 		fileDialog := dialog.NewFileOpen(func(reader fyne.URIReadCloser, err error) {
 			if err != nil {
+				dialog.ShowError(err, myWindow)
 				log.Println("Failed to open file:", err)
 				return
 			}
 			if reader == nil {
+				dialog.ShowInformation("No file selected", "Please select a file", myWindow)
 				log.Println("No file selected")
 				return
 			}
@@ -58,27 +64,55 @@ func startUI() {
 	})
 
 	startButton := widget.NewButton("Start Download", func() {
+		if isDownloading {
+			dialog.ShowInformation("Download in Progress", "A download is already in progress", myWindow)
+			return
+		}
+
 		filePath := fileEntry.Text
 		if filePath == "" {
+			dialog.ShowInformation("No file selected", "Please select a file to download", myWindow)
 			log.Println("No file selected for download")
 			return
 		}
-		startDownload(filePath)
+
+		isDownloading = true
+		go func() {
+			defer func() { isDownloading = false }()
+			startDownload(filePath, myWindow)
+		}()
 	})
 
-	content := container.NewVBox(label, fileEntry, fileButton, startButton)
+	// Implement drag and drop
+	dropArea := canvas.NewRectangle(&color.RGBA{R: 200, G: 200, B: 200, A: 255})
+	dropArea.SetMinSize(fyne.NewSize(400, 200))
+	dropLabel := widget.NewLabel("Drop .torrent file here")
+
+	dropContainer := container.NewStack(dropArea, container.NewCenter(dropLabel))
+
+	myWindow.SetOnDropped(func(pos fyne.Position, uris []fyne.URI) {
+		if len(uris) > 0 {
+			fileEntry.SetText(uris[0].Path())
+			dialog.ShowInformation("File Selected", fmt.Sprintf("Selected file: %s", uris[0].Path()), myWindow)
+		}
+	})
+
+	content := container.NewVBox(label, fileEntry, fileButton, startButton, dropContainer)
 	myWindow.SetContent(content)
 	myWindow.ShowAndRun()
 }
 
 func startCLI() {
 	inPath := os.Args[1]
-	startDownload(inPath)
+	startDownload(inPath, nil)
 }
 
-func startDownload(filePath string) {
+func startDownload(filePath string, myWindow fyne.Window) {
 	tf, err := core.Open(filePath)
 	if err != nil {
+		if myWindow != nil {
+			dialog.ShowError(err, myWindow)
+		}
 		log.Fatal(err)
 	}
 
@@ -87,14 +121,19 @@ func startDownload(filePath string) {
 
 	err = tf.DownloadTorrent()
 	if err != nil {
+		if myWindow != nil {
+			dialog.ShowError(err, myWindow)
+		}
 		log.Fatal(err)
 	}
 
 	duration := time.Since(startTime)
-
 	minutes := int(duration.Minutes())
 	seconds := int(duration.Seconds()) % 60
 
-	fmt.Printf("Download completed in %d minutes and %d seconds.\n", minutes, seconds)
+	message := fmt.Sprintf("Download completed in %d minutes and %d seconds.\n", minutes, seconds)
+	if myWindow != nil {
+		dialog.ShowInformation("Download Completed", message, myWindow)
+	}
 	log.Printf("Downloaded in %v \n", duration)
 }
